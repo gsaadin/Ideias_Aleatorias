081b018e0619e85fbf8df3141796085e7c43cd09
Guilherme Saad <105023562+guilhermesaad@users.noreply.github.com>
2022-11-04 15:42:31 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import time
+
+
+def main():
+    A = [1]
+
+    ini = time.time()
+    Classificador(A)
+    fim = time.time()
+    with open('1elementos.txt', 'a') as arquivo:
+        arquivo.write(str(' Tempo: '))
+        arquivo.write(str(fim - ini))
+
+
+def ProdCart(a, b):
+    c = []
+    for i in range(len(a)):
+        for j in range(len(b)):
+            c.append((a[i], b[j]))
+    return c
+
+
+def PartesDe(a):
+    resultado = [[]]
+    for i in a:
+        resultado += [sub + [i] for sub in resultado]
+    return resultado
+
+
+def Reflexiva(rel, A):
+    for i in A:
+        if not (i, i) in rel:
+            return " "
+    return "R"
+
+
+def Simetrica(rel, A):
+    for i in A:
+        for j in A:
+            if (i, j) in rel and not (j, i) in rel:
+                return " "
+    return "S"
+
+
+def Transitiva(rel, A):
+    for i in A:
+        for j in A:
+            for k in A:
+                if (i, j) in rel and (j, k) in rel and not (i, k) in rel:
+                    return " "
+    return "T"
+
+
+def Classifica(rel, A):
+    S = Reflexiva(rel, A) + Transitiva(rel, A) + Simetrica(rel, A)
+
+    if S == "RTS":
+        S += "E"
+    return S
+
+
+def Classificador(A):
+    for rel in PartesDe(ProdCart(A, A)):
+        with open('1elementos.txt', 'a') as arquivo:
+            arquivo.write(str(Classifica(rel, A)) + '\n')
+
+
+if __name__ == "__main__":
+    main()
